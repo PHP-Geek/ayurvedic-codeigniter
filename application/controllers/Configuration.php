c4e6208d24a90bdc877d95342c6b8c7b3b6faeee
PHP Geek <PHP-Geek@users.noreply.github.com>
2017-03-21 18:14:38 +0530

    Update Configuration.php

--- application/controllers/Configuration.php
+++ application/controllers/Configuration.php
@@ -11,7 +11,7 @@ class Configuration extends MY_Controller {
         $this->load->model('Configuration_model');
     }
 
-    function category(){
+    function category() {
         parent::allow(array('administrator'));
         $data = array();
         parent::render_view($data);
@@ -20,54 +20,15 @@ class Configuration extends MY_Controller {
     function product_datatable() {
         parent::allow(array('administrator'));
         $this->load->library('Datatables');
-        $this->datatables->select('product_id, category_name, product_name, product_description, product_is_display, product_image, product_quantity, product_sale_price, product_status')->from('products');
+        $this->datatables->select("product_id, category_name, product_name, product_description, product_quantity, IF(product_image_1 = '', '', (CONCAT('" . base_url() . 'uploads/products/' . "',DATE_FORMAT(product_created,'%Y/%m/%d/%H/%i/%s/'),product_image_1))) AS image_url_1, IF(product_image_2 = '', '', (CONCAT('" . base_url() . 'uploads/products/' . "',DATE_FORMAT(product_created,'%Y/%m/%d/%H/%i/%s/'),product_image_2))) AS image_url_2, product_is_display, product_status")->from('products');
         $this->datatables->join('categories', 'categories.category_id=products.product_id', 'left');
         $this->datatables->where('products.product_status != -1');
         echo $this->datatables->generate();
     }
 
-    function product() {
+    function list_product() {
         parent::allow(array('administrator'));
         $data = array();
-        parent::render_view($data);
-    }
-
-    function add_product() {
-        parent::allow(array('administrator'));
-        $data = array();
-        $this->load->helper('form');
-        if ($this->input->post()) {
-            $this->load->model('Product_model');
-            $this->load->library('form_validation');
-            $this->form_validation->set_rules('category_id', 'Category Id', 'trim|required');
-            $this->form_validation->set_rules('product_name', 'Product Name', 'trim|required|is_unique[products.product_name]');
-            $this->form_validation->set_rules('product_description', 'Product Description', 'trim|required');
-            $this->form_validation->set_rules('product_image_file_name', 'Product Image', 'trim|required');
-            $this->form_validation->set_error_delimiters('', '<br />');
-            if ($this->form_validation->run()) {
-                $time_now = date('Y-m-d H:i:s');
-                $products_insert_array = array(
-                    'categories_id' => $this->input->post('category_id'),
-                    'product_name' => $this->input->post('product_name'),
-                    'product_slug' => url_title($this->input->post('product_name'), '-', TRUE),
-                    'product_description' => $this->input->post('product_description'),
-                    'product_is_display' => $this->input->post('product_is_display'),
-                    'product_image' => $this->input->post('product_image_file_name'),
-                    'product_quantity' => '',
-                    'product_sale_price' => $this->input->post('product_sale_price'),
-                    'product_status' => '1',
-                    'product_created' => $time_now,
-                );
-                if ($this->Product_model->add_product($products_insert_array)) {
-                    die('1');
-                }
-            } else {
-                echo validation_errors();
-                die;
-            }
-            die('0');
-        }
-        $data['category_details_array'] = $this->Configuration_model->get_all_categories();
         parent::render_view($data);
     }
 
@@ -121,51 +82,92 @@ class Configuration extends MY_Controller {
         die('0');
     }
 
-    function edit_product($product_id = 0) {
+    function product($product_id = 0) {
         parent::allow(array('administrator'));
-        if($product_id !== 0){
-            $this->load->model('Product_model');
-            $data = array();
+        $data = array();
+        $time_now = date('Y-m-d H:i:s');
+        $this->load->model('Product_model');
+        if ($product_id !== 0) {
+            $data['product_quantities_details_array'] = $this->Product_model->get_product_quantities_by_product_id($product_id);
             $data['product_details_array'] = $this->Product_model->get_product_by_id($product_id);
-            if(count($data['product_details_array']) > 0 && $data['product_details_array']['product_status'] == '1') {
-                $this->load->helper('form');
-                if ($this->input->post()) {
-                    $this->load->library('form_validation');
-                    $this->form_validation->set_rules('category_id', 'Categories Id', 'trim|required');
-                    $this->form_validation->set_rules('product_name', 'Product Name', 'trim|required');
-                    $this->form_validation->set_rules('product_description', 'Product Description', 'trim|required');
-                    $this->form_validation->set_rules('product_image_file_name', 'Product Image', 'trim|required');
-                    $this->form_validation->set_error_delimiters('', '<br />');
-                    if ($this->form_validation->run()) {
-                        $time_now = date('Y-m-d H:i:s');
-                        $product_edit_array = array (
-                            'categories_id' => $this->input->post('category_id'),
-                            'product_name' => $this->input->post('product_name'),
-                            'product_slug' => url_title($this->input->post('product_name'), '-', TRUE),
-                            'product_description' => $this->input->post('product_description'),
-                            'product_is_display' => ($this->input->post('product_is_display') == 'true') ? '1' : '0',
-                            'product_image' => $this->input->post('product_image_file_name'),
-                            'product_quantity' => '',
-                            'product_sale_price' => $this->input->post('product_sale_price'),
-                            'product_status' => '1',
-                            'product_modified' => $time_now,
-                        );
-                        if ($this->Product_model->edit_product_by_id($product_id,$product_edit_array)) {
-                            die('1');
-                        }
-                    } else {
-                        echo validation_errors();
-                        die;
-                    }
-                    die('0');
-                }
-
-                $data['category_details_array'] = $this->Configuration_model->get_all_categories();
-                parent::render_view($data);
-                return;
+            if (count($data['product_details_array']) == 0) {
+                redirect('configuration/list_product', 'refresh');
             }
+            $data['product_details_array']['product_modified'] = $time_now;
+        } else {
+            $data['product_details_array']['product_quantity'] = 0;
+            $data['product_details_array']['product_status'] = 1;
+            $data['product_details_array']['product_created'] = $time_now;
         }
-        redirect('configuration/product','refresh');
+        $this->load->helper('form');
+        if ($this->input->post()) {
+            $this->load->library('form_validation');
+            $this->form_validation->set_rules('category_id', 'Categories Id', 'trim|required');
+            $this->form_validation->set_rules('product_name', 'Product Name', 'trim|required');
+            $this->form_validation->set_rules('product_description', 'Product Description', 'trim|required');
+            $this->form_validation->set_rules('product_quantity_volume[]', 'Product Sale Price Volume', 'trim|required');
+            $this->form_validation->set_rules('product_images[]', 'Product Image', 'trim|required');
+            $this->form_validation->set_error_delimiters('', '<br />');
+            if ($this->form_validation->run()) {
+                foreach ($this->input->post('product_quantity_volume') as $key => $product_quantity_volume) {
+                    if ($product_quantity_volume != "" || $this->input->post('unit_id')[$key] != "" || $this->input->post('product_quantity_price')[$key] != "") {
+                        if ($product_quantity_volume == "" || $this->input->post('unit_id')[$key] == "" || $this->input->post('product_quantity_price')[$key] == "") {
+                            die('Please fill Sale price for particular volume.');
+                        }
+                    }
+                }
+                foreach ($this->input->post('product_images') as $key => $product_image) {
+                    $upload_thumbnail[$key] = $product_image;
+                    if (is_file(FCPATH . 'uploads/' . $upload_thumbnail[$key])) {
+                        $upload_image_directory = FCPATH . 'uploads/products/' . date('Y/m/d/H/i/s', strtotime($time_now));
+                        if (!is_dir($upload_image_directory)) {
+                            mkdir($upload_image_directory, 0777, TRUE);
+                        }
+                        if (parent::resize_image(FCPATH . 'uploads/' . $upload_thumbnail[$key], $upload_image_directory . '/' . $upload_thumbnail[$key], 150, 150)) {
+                            unlink(FCPATH . 'uploads/' . $upload_thumbnail[$key]);
+                        }
+                    }
+                }
+                $data['product_details_array']['categories_id'] = $this->input->post('category_id');
+                $data['product_details_array']['product_name'] = $this->input->post('product_name');
+                $data['product_details_array']['product_slug'] = url_title($this->input->post('product_name'), '-', TRUE);
+                $data['product_details_array']['product_description'] = $this->input->post('product_description');
+                $data['product_details_array']['product_is_display'] = ($this->input->post('product_is_display') == 1 ? 1 : 0);
+                $data['product_details_array']['product_image_1'] = $upload_thumbnail[0];
+                $data['product_details_array']['product_image_2'] = isset($upload_thumbnail[1]) ? $upload_thumbnail[1] : "";
+                if ($product_id == 0) {
+                    $product_id = $this->Product_model->save($data['product_details_array']);
+                }
+                if ($product_id > 0) {
+                    $this->Product_model->edit_product_quantities_by_product_id($product_id, array(
+                        'product_quantity_status' => '-1',
+                        'product_quantity_modified' => $time_now
+                    ));
+                    if (count($this->input->post('product_quantity_volume')) > 0) {
+                        foreach ($this->input->post('product_quantity_volume') as $key => $product_quantity_volume) {
+                            $product_quantity_insert_array[] = array(
+                                'products_id' => $product_id,
+                                'units_id' => $this->input->post('unit_id')[$key],
+                                'product_quantity_volume' => $product_quantity_volume,
+                                'product_quantity_price' => $this->input->post('product_quantity_price')[$key],
+                                'product_quantity_status' => 1,
+                                'product_quantity_created' => $time_now
+                            );
+                        }
+                        $this->Product_model->add_product_quantities_batch($product_quantity_insert_array);
+                        die('1');
+                    }
+                }
+            } else {
+                echo validation_errors();
+                die;
+            }
+            die('0');
+        }
+        $data['category_details_array'] = $this->Configuration_model->get_all_categories();
+        $data['units_array'] = $this->Configuration_model->get_all_units();
+        parent::render_view($data);
+        return;
     }
 
     function category_datatable() {
@@ -303,7 +305,7 @@ class Configuration extends MY_Controller {
             if ($this->form_validation->run()) {
                 $this->load->model('Product_model');
                 $product_details_array = $this->Product_model->get_product_by_id($this->input->post('product_id'));
-                if(count($product_details_array) > 0) {
+                if (count($product_details_array) > 0) {
                     $product_edit_array = array(
                         'product_status' => '-1',
                         'product_modified' => date('Y-m-d H:i:s')
