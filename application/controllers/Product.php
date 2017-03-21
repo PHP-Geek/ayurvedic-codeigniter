cbc1824182cd071d2489fd28e326e5f5ddab43b1
PHP Geek <PHP-Geek@users.noreply.github.com>
2017-03-21 18:15:30 +0530

    Update Product.php

--- application/controllers/Product.php
+++ application/controllers/Product.php
@@ -12,5 +12,12 @@ class Product extends MY_Controller {
         parent::__construct();
         $this->load->model('Product_model');
     }
+    
+    function get_product_by_id() {
+        parent::allow(array('administrator'));
+        parent::json_output(array('product_details' => $this->Product_model->get_product_by_id($this->input->post('product_id')),
+            'product_quantity_details' => $this->Product_model->get_product_quantities_by_product_id($this->input->post('product_id'))));
+        return;
+    }
 
 }
